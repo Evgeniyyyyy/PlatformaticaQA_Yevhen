60d25f1455abaaa42deddab37ccebec764976c5e
YBozhenko <ebozhenko@ukr.net>
2021-08-19 14:06:05 +0300

    some commit

--- src/test/java/GroupJavaBearsTest.java
+++ src/test/java/GroupJavaBearsTest.java
@@ -1,53 +1,39 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
+import base.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
-public class GroupJavaBearsTest {
-    @BeforeClass
-    public void beforeClass() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    private WebDriver driver;
-
-    @BeforeMethod
-    public void beforeTest() {
-        driver = new ChromeDriver();
-    }
+public class GroupJavaBearsTest extends BaseTest {
 
     @Test
     public void reviewTest() throws InterruptedException {
-        driver.get("https://shop.demoqa.com/my-account/");
+        getDriver().get("https://shop.demoqa.com/my-account/");
         Thread.sleep(2000);
-        driver.findElement(By.linkText("Lost your password?")).click();
+        getDriver().findElement(By.linkText("Lost your password?")).click();
         Thread.sleep(2000);
-        driver.navigate().to("https://shop.demoqa.com/my-account/");
+        getDriver().navigate().to("https://shop.demoqa.com/my-account/");
         Thread.sleep(2000);
-        driver.navigate().back();
+        getDriver().navigate().back();
         Thread.sleep(2000);
-        driver.navigate().forward();
+        getDriver().navigate().forward();
         Thread.sleep(2000);
-        driver.navigate().refresh();
+        getDriver().navigate().refresh();
 
 
     }
-    public static void newClick (WebDriver driver, WebElement element){
-        JavascriptExecutor executor = (JavascriptExecutor)driver;
+
+    public static void newClick(WebDriver driver, WebElement element) {
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
         executor.executeScript("arguments[0].click();", element);
     }
 
     @Test
     public void Test2() throws InterruptedException {
-        driver.get("https://demoqa.com/automation-practice-form");
+        getDriver().get("https://demoqa.com/automation-practice-form");
        /* driver.findElement(By.id("firstName")).sendKeys("John");
         Thread.sleep(2000);
         driver.findElement(By.id("lastName")).sendKeys("Parker");
@@ -62,26 +48,20 @@ public class GroupJavaBearsTest {
         Thread.sleep(2000);*/
         //textName.sendKeys("BrowN");
         // Thread.sleep(2000);
-      //  driver.findElement(By.xpath("//input[@name='gender'][@value='Male']")).click();
+        //  driver.findElement(By.xpath("//input[@name='gender'][@value='Male']")).click();
 
         //Collection with Test
-        List<WebElement> gender = driver.findElements(By.id("//input[@type='radio']"));
+        List<WebElement> gender = getDriver().findElements(By.id("//input[@type='radio']"));
         gender.size();
         for (int i = 0; i < gender.size(); i++) {
             String value = gender.get(i).getAttribute("value");
-            if (value.equalsIgnoreCase("male")){
-             //   gender.get(i).click();
-                newClick(driver, gender.get(i));
+            if (value.equalsIgnoreCase("male")) {
+                //   gender.get(i).click();
+                newClick(getDriver(), gender.get(i));
                 break;
             }
-            Thread.sleep(5000);
         }
 
-    }
-
-    @AfterMethod
-    public void afterTest() {
-        driver.quit();
     }
 
 }
---
+++ src/test/java/utils/TestUtils.java
@@ -0,0 +1,20 @@
+package utils;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class TestUtils {
+    public static void jsClick (WebDriver driver, WebElement element){
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", element);
+    }
+    public static void scroll (WebDriver driver, WebElement element){
+        //to do
+        throw new RuntimeException("Method is not implemented");
+    }
+    public static void scrollClick (WebDriver driver, WebElement element){
+        scroll(driver, element);
+        element.click();
+    }
+}
